2c1005a4a4f10d3d6a89c1eb54d7adb6338aebae
AEToluca <aeestrada1004@gmail.com>
2025-03-28 11:49:45 -0400

    All methods completed and tested

--- Data Structures/BuildingBinaryHeap/.venv/BinaryHeap.py
+++ Data Structures/BuildingBinaryHeap/.venv/BinaryHeap.py
@@ -4,18 +4,26 @@ class BinaryHeap:
         
 
     def insert(self, value):
+            #add value to the end then sift up
             self.heap_list.append(value)
             self.sift_up()
 
     def remove_min(self) -> int:
-        pass
-
+        #raise an exception if the heap is empty
+        if self.size() <= 0:
+            raise ("Heap is empty")
+        else:
+            #swap the first and last elements, put last element in a variable,sift down and return the variable
+            self.heap_list[self.size() - 1], self.heap_list[0] = self.heap_list[0], self.heap_list[self.size() - 1]
+            result = self.heap_list.pop()
+            self.sift_down()
+            return result
     def sift_up(self):
-        
+        #set current to the last index
         current = self.size() - 1
-        while current > 0:
+        while current > 0: #go until current is root
             parent = (current - 1) // 2
-
+            #swap if parent is greater than current
             if self.heap_list[parent] > self.heap_list[current]:
                 self.heap_list[parent], self.heap_list[current] = self.heap_list[current], self.heap_list[parent]
                 current = parent
@@ -23,9 +31,9 @@ class BinaryHeap:
                 break
     def sift_down(self):
         #return if heap only has 1 or less elements
-        if self.size <= 1:
+        if self.size() <= 1:
             return
-        last = self.size()
+        last = self.size() - 1
         #swap first and last items
         self.heap_list[0], self.heap_list[last] = self.heap_list[last], self.heap_list[0]
 
@@ -48,25 +56,24 @@ class BinaryHeap:
 
             self.heap_list[current], self.heap_list[smallest] = self.heap_list[smallest], self.heap_list[current]
             current = smallest
-
+    #return the root node
     def find_min(self) -> int:
         return self.heap_list[0]
 
+    #reutrn the size of the heap
     def size(self) -> int:
         return len(self.heap_list)
 
-    def isFull(self) -> bool:
-        return True
 
 def  main():
     test = BinaryHeap() 
+    test.insert(7)
     test.insert(4)
     test.insert(5)
-    test.insert(2)
-   
+    test.insert(1)
     test.insert(3)
+    print(test.remove_min())
     print(test.find_min())
-    print(test.size())
 
 if __name__ == "__main__":
     # Driver code to test your heap
